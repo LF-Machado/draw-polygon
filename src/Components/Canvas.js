58110dcb63175b3c6bd673f74d1705d8d12f4e5c
Luis Felipe Machado <luis.felipe.machado13@gmail.com>
2021-05-30 19:07:49 -0500

    Fixed bug for different screen sizes
    Bug where polygon would not display correctly in different sizes

--- src/Components/Canvas.js
+++ src/Components/Canvas.js
@@ -68,12 +68,10 @@ function Canvas() {
     const { width, height } = canvas.getBoundingClientRect();
 
     if (canvas.width !== width || canvas.height !== height) {
-      const { devicePixelRatio: ratio = 1 } = window;
-      const context = canvas.getContext("2d");
-      canvas.width = width * ratio;
-      canvas.height = height * ratio;
-      context.scale(ratio, ratio);
-      return true;
+      canvas.width = width;
+      canvas.height = height;
+      return true; // here you can return some usefull information like delta width and delta height instead of just true
+      // this information can be used in the next redraw...
     }
 
     return false;
